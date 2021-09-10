340692b0fdfa6ec30a56a5e063dddcbf05086daf
Sam Roble <smroble1996@gmail.com>
2021-09-10 11:45:02 -0600

    what I got done

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,14 @@
 puts "Deleting old data..."
 Product.destroy_all
 User.destroy_all
+Review.destroy_all
 
 puts "Creating users..."
 user1 = User.create(name: Faker::Name.name)
 user2 = User.create(name: Faker::Name.name)
 user3 = User.create(name: Faker::Name.name)
+
+#This is the part that really tripped me up. I studied the movies challenge last night, and this was different.I am not sure how to migrate lines 9 through 11 with what I was trying to do on line 23#
 
 puts "Creating products..."
 product1 = Product.create(name: "Stapler", price: 10)
@@ -17,10 +20,7 @@ product4 = Product.create(name: "Ballpoint Pens", price: 2)
 product5 = Product.create(name: "Scotch Tape", price: 3)
 
 puts "Creating reviews..."
-# *****************************************************************
-# * TODO: create reviews! Remember, a review belongs to a product *
-# * and a review belongs to an user.                              *
-# *****************************************************************
-# Create Reviews Here
+thing_one = Review.create(comment: "Thing One", star_rating: 2, product_id: product1.id)
+
 
 puts "Seeding done!"
